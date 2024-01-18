3e3de0cec12bab7ac032439e68202c5d077f02ad
Ye Feng <ye@evercoast.com>
2024-01-19 09:28:13 +1300

    Add xosbuild.sh and xrossimbuild.sh

--- xcframeworkbuild.sh
+++ xcframeworkbuild.sh
@@ -2,7 +2,7 @@
 #
 # This script generates 'WebP.xcframework', 'WebPDecoder.xcframework',
 # 'WebPDemux.xcframework' and 'WebPMux.xcframework'.
-# An iOS, Mac or Mac Catalyst app can decode WebP images by including
+# An iOS, visionOS, Mac or Mac Catalyst app can decode WebP images by including
 # 'WebPDecoder.xcframework' and both encode and decode WebP images by including
 # 'WebP.xcframework'.
 #
